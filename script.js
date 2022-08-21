228aa0521028eda654204bcc07f1a0305783cf90
scootero <61133217+scootero@users.noreply.github.com>
2022-08-21 18:10:31 -0500

    Add files via upload
    pre-meeting and added tickers

--- script.js
+++ script.js
@@ -14,10 +14,9 @@
 
 const menuBars = document.getElementById("menu-bars");
 
-
 const navCompanyName = document.getElementById("nav-company-name");
 
-const slides = document.getElementsByClassName('slides');
+const slides = document.getElementsByClassName("slides");
 
 const product1 = document.getElementById("product1");
 const product2 = document.getElementById("product2");
@@ -26,8 +25,6 @@ const slide1 = document.getElementById("slide1");
 const slide2 = document.getElementById("slide2");
 const slide3 = document.getElementById("slide3");
 
-
-
 function togglenav() {
   menuBars.classList.toggle("menu-active");
   menuBars.classList.toggle("change");
@@ -35,48 +32,32 @@ function togglenav() {
 
 menuBars.addEventListener("click", togglenav);
 
-
-
-const miniImgs = document.getElementById('mini-imgs').children;
-for (let miniImg of miniImgs) { 
+const miniImgs = document.getElementById("mini-imgs").children;
+for (let miniImg of miniImgs) {
   miniImg.addEventListener("click", () => {
-    for(let slide of slides) { 
+    for (let slide of slides) {
       slide.style.display = "none";
     }
-  })
-  };
-
-  product1.addEventListener("click", () => {
-    slide1.style.display = "block";
   });
-  product2.addEventListener("click", () => {
-    slide2.style.display = "block";
-  });
+}
 
-  product3.addEventListener("click", () => {
-    slide3.style.display = "block";
-  });
-  // miniImg.addEventListener("click", () => {
-  //   console.log('mini clicked')
+product1.addEventListener("click", () => {
+  slide1.style.display = "block";
+});
+product2.addEventListener("click", () => {
+  slide2.style.display = "block";
+});
 
-  //   slides.forEach(slide => {
-  //     slide.style.display = "none";
-  //   }
-  // )}
+product3.addEventListener("click", () => {
+  slide3.style.display = "block";
+});
+// miniImg.addEventListener("click", () => {
+//   console.log('mini clicked')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//   slides.forEach(slide => {
+//     slide.style.display = "none";
+//   }
+// )}
 
 let slideIndex = 1;
 showSlides(slideIndex);
